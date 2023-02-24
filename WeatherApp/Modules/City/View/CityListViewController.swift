4def3d6b2e9e60ef04a7d01fb6caa99e3f7ab50f
“Nishant <nishaantshukla23@gmail.com>
2023-02-25 00:10:38 +0530

    Added App Icon & Resources Json, launchScreen setup.

---
+++ WeatherApp/Modules/City/View/CityListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CityListViewController.swift
+//  WeatherApp
+//
+//  Created by N Shukla on 24/02/23.
+//
+
+import UIKit
+
+class CityListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
